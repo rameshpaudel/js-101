11417a7a7cf1883ceee848a1b1534b6c604ffad5
Ramesh Paudel <rpaudel58@gmail.com>
2019-05-08 15:26:23 +0545

    Completed basic calculator

---
+++ js/arithmetic.js
@@ -0,0 +1,19 @@
+
+        // Arithmetic functions
+        // =============================
+
+        function add(a, b) {
+            return a + b;
+        }
+
+        function substract(a, b) {
+            return a - b;
+        }
+
+        function multiply(a, b) {
+            return a * b;
+        }
+
+        function divide(a, b) {
+            return a / b;
+        }
