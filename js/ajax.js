f07a3b79ae915273aa3dfc9eed5262df8698317b
Ramesh Paudel <rpaudel58@gmail.com>
2019-05-30 15:05:09 +0545

    Ajax request made and generated html output from the remote data

--- js/ajax.js
+++ js/ajax.js
@@ -1,15 +1,34 @@
 
 var outputBox = document.querySelector("#output")
+var generatedBox = document.querySelector("#generated")
 var fetchFile = new XMLHttpRequest();
 
 //This event is fired when the ajax request is successful
 fetchFile.onload= function(){
+    //We can manipulate the response that we got from the server
+    var parsedValue = JSON.parse(this.response);
+
     //this represents the XMLHttpRequest object that we have instanciated
-    // console.log("The request object",this)
-    console.log(this.response)
+    console.log("The request object",this)
+
+    console.log(parsedValue)
+
+    // Manipulating the data
+
+    if(parsedValue.data && parsedValue.data.length){
+        for(var i= 0; i < parsedValue.data.length; i++){
+            //Genearate the output
+
+            generatedBox.innerHTML += generateList(parsedValue.data[i]); ///generateList is custom function
+        }
+    }
+
+
+
+
 
     //The response from the server 
-    outputBox.innerText = this.response;
+    // outputBox.innerText = this.response;
     
 }
 
@@ -18,7 +37,7 @@ fetchFile.onreadystatechange = function(e){
     //ReadyState property changes from 4 times 
 
     if(this.readyState === 3){
-        
+
     }
     // console.log('The event',e)
     //this.statusText -> Checks the status in text format
@@ -27,7 +46,22 @@ fetchFile.onreadystatechange = function(e){
 }
 
 document.querySelector("#make-request").addEventListener('click',()=>{
-    fetchFile.open("GET","http://localhost/test.txt");
+    fetchFile.open("GET","https://reqres.in/api/users?page=2");
     fetchFile.send();
 })
 
+
+
+function generateList(obj){
+    return `
+        <div class="col-md-4">
+            <div class="col-md-4">
+                <img src="${obj.avatar}" >
+            </div>
+            <div class="col-md-8">
+                <h3>${obj.first_name} ${obj.last_name}</h3>
+                <a href="mailto:${obj.email}">${obj.email}</a>
+            </div>
+        </div>
+    `
+}
